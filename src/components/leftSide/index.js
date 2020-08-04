cb8e692f8d26cf16a74eb2b702dcc7dbe877f8de
Hannah Lima <nanaa_lima@hotmail.com>
2020-08-04 13:41:10 -0500

    ohms calculator

---
+++ src/components/leftSide/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LeftSide.css";
+
+function LeftSide() {
+  return (
+    <div className="leftSide">
+      <h1>This is the left side.</h1>
+    </div>
+  );
+}
+
+export default LeftSide;
---
+++ src/components/rightSide/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./RightSide.css";
+
+function RightSide() {
+  return (
+    <div className="rightSide">
+      <h1>This is the right side.</h1>
+    </div>
+  );
+}
+
+export default RightSide;
